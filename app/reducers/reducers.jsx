7df5050fe322f055f68f84ab3cf8a2bf36de0404
[jsw324] <jwalkow@tallience.com>
2017-04-17 12:11:33 -0400

    Add authReducer and actions

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -47,3 +47,16 @@
         return state;
     }
   };
+
+  export var authReducer = (state = {}, action) => {
+    switch (action.type) {
+      case 'LOGIN':
+        return {
+          uid: action.uid
+        };
+      case 'LOGOUT':
+        return {};
+      default:
+        return state;
+    };
+  };
